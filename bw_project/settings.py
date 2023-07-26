fb2e556559e1728c1ad777b1ee1ae07fc90b7800
malachai <malachai@onwona.com>
2023-07-26 10:15:05 -0400

    Adding different pages for 404 and 500 errors

--- bw_project/settings.py
+++ bw_project/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'bw_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
